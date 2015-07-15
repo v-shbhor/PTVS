584d70965d62d6e5111afbdb2a9e0add06cf65ae
Shailesh <shabho@live.com>
2015-07-15 14:22:45 +0000

    for and while

---
+++ PythonApplication1/turtule_for_while.py
@@ -0,0 +1,47 @@
+import turtle
+#turtle.forward(100)
+#turtle.right(90)
+#turtle.forward(100)
+
+
+#for steps in range(4):
+#    turtle.forward(100)
+#    turtle.right(90)
+
+#for steps in range(4):
+#    turtle.forward(100)
+#    turtle.right(90)
+#    for moresteps in range(4):
+#        turtle.forward(50)
+#        turtle.right(90)
+
+# run loop for 1,2 and 3
+#for steps in range(1,4) :
+#    print(steps)
+
+# run loop for 1 to 10 and jump by 2 each time
+#for steps in range(1,10,2) :
+#    print(steps)
+
+#without range attribute
+#for steps in [1,12,10,4,5] :
+#    print (steps)
+
+#for steps in ['red','blue','green','black'] :
+#    turtle.color(steps)
+#    turtle.forward(100)
+#    turtle.right(90)
+
+
+
+counter = 0
+while counter < 4 :
+    turtle.forward(100)
+    turtle.right(90)
+    counter = counter + 1
+
+#there are also for else and while else statements need to search on google
+
+    
+
+
